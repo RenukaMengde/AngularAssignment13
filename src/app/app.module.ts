504bd0251abc357213f7e92b636a077e8787369d
RenukaMengde <128237247+RenukaMengde@users.noreply.github.com>
2023-05-06 20:48:23 +0530

    Add files via upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ContactDetailsComponent } from './contact-details/contact-details.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ContactDetailsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgbModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
